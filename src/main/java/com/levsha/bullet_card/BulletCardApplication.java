a33331556216e77164489dfdb5b827a152e81d4b
DanilYusupov <danilka5318@yandex.ru>
2018-10-21 19:02:12 +0300

    Initial commit

---
+++ src/main/java/com/levsha/bullet_card/BulletCardApplication.java
@@ -0,0 +1,12 @@
+package com.levsha.bullet_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletCardApplication.class, args);
+    }
+}
